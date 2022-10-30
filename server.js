37809ff375a0ea8c88c000fb11d1470cc0bced7a
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-30 05:40:25 -0400

    setup of modules in server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// Global Modules
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+// Third Party Modules
+const express = require('express');
+
+// Local Modules
+// const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+// const uuid = require('./helpers/uuid');
+
+// Global Variables
+const PORT = 3001;
+const app = express();
