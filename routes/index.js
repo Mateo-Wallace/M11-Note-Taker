a97d2268ca21db398f861b452ce303a6bcff78ac
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-05 07:33:10 -0400

    connected api route

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
-// const express = require('express');
+const express = require('express');
 
 // // Import our modular routers for /tips and /feedback
-// const tipsRouter = require('./tips');
-// const feedbackRouter = require('./feedback');
-// const diagnosticsRouter = require('./diagnostics');
+const notesRouter = require('./notes');
 
-// const app = express();
+const app = express();
 
-// app.use('/tips', tipsRouter);
-// app.use('/feedback', feedbackRouter);
-// app.use('/diagnostics', diagnosticsRouter);
+// app.use('/notes', notesRouter);
 
-// module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Global Modules
 const fs = require('fs');
 const path = require('path');
-const util = require('util');
 
 // Third Party Modules
 const express = require('express');
@@ -14,9 +13,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // // Middleware for parsing JSON and urlencoded form data
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 // Connects all files in public folder. Ex. html to css to js
 app.use(express.static('public'));
@@ -31,10 +30,7 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
-
-
-
+// Listens for requests to serve to client
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
