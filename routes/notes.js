059763d8f9aba7d3a0b8bdec58f7bd935a186928
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-05 08:13:45 -0400

    notes displaying on side of page

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,10 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-// // GET Route for retrieving all the tips
-// tips.get('/', (req, res) => {
-//   readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-// });
-
-// // GET Route for a specific tip
-// tips.get('/:tip_id', (req, res) => {
-//   const tipId = req.params.tip_id;
-//   readFromFile('./db/tips.json')
-//     .then((data) => JSON.parse(data))
-//     .then((json) => {
-//       const result = json.filter((tip) => tip.tip_id === tipId);
-//       return result.length > 0
-//         ? res.json(result)
-//         : res.json('No tip with that ID');
-//     });
-// });
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 // POST Route for a new UX/UI tip
 notes.post('/', (req, res) => {
