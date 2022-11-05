44996e9a0ee43b52e5b084238e3f240d91c93bf6
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-05 07:09:01 -0400

    server pulling up index and notes html

--- server.js
+++ server.js
@@ -7,9 +7,34 @@ const util = require('util');
 const express = require('express');
 
 // Local Modules
-// const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
-// const uuid = require('./helpers/uuid');
+const api = require('./routes/index.js')
 
 // Global Variables
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// // Middleware for parsing JSON and urlencoded form data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+// Connects all files in public folder. Ex. html to css to js
+app.use(express.static('public'));
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
